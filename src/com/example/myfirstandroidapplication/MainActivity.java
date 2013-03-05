e91bf54827fe324da1092da40a92b9618ad43142
Adrian Ignat <adrian.ignat@gmail.com>
2013-03-05 22:35:47 +0200

    Added markers on the map; crashed application

--- src/com/example/myfirstandroidapplication/MainActivity.java
+++ src/com/example/myfirstandroidapplication/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.myfirstandroidapplication;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -14,6 +21,10 @@ public class MainActivity extends Activity
 {
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
+	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+	static final LatLng KIEL = new LatLng(53.551, 9.993);
+	private GoogleMap map;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
@@ -42,13 +53,30 @@ public class MainActivity extends Activity
 	
 	public void sendMessage(View view)
 	{
-		Intent intent = new Intent(this, DisplayMessageActivity.class);
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
+		//Intent intent = new Intent(this, DisplayMessageActivity.class);
+		//EditText editText = (EditText) findViewById(R.id.edit_message);
+		//String message = editText.getText().toString();
 		
-		intent.putExtra(EXTRA_MESSAGE, message);
+		//intent.putExtra(EXTRA_MESSAGE, message);
 		
-		startActivity(intent);
+		//startActivity(intent);
+		
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+		        .getMap();
+		    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+		        .title("Hamburg"));
+		    Marker kiel = map.addMarker(new MarkerOptions()
+		        .position(KIEL)
+		        .title("Kiel")
+		        .snippet("Kiel is cool")
+		        .icon(BitmapDescriptorFactory
+		            .fromResource(R.drawable.ic_launcher)));
+
+		    // Move the camera instantly to hamburg with a zoom of 15.
+		    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+
+		    // Zoom in, animating the camera.
+		    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
 	}
 
 }
