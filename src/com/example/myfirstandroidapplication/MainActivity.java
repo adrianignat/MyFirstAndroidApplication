a3134ec0e9c525b19003df5ddf0fd076dae6799c
Adrian Ignat <adrian.ignat@gmail.com>
2013-03-05 15:37:19 +0200

    Initial Commit

---
+++ src/com/example/myfirstandroidapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.myfirstandroidapplication;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity 
+{
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		
+		int playServiceAvailability = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		
+		if (playServiceAvailability != ConnectionResult.SUCCESS)
+			GooglePlayServicesUtil.getErrorDialog(playServiceAvailability, this, 0);
+	}
+	
+	public void sendMessage(View view)
+	{
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		
+		intent.putExtra(EXTRA_MESSAGE, message);
+		
+		startActivity(intent);
+	}
+
+}
